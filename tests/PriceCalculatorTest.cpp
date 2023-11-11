3ebe3c092fb15a1e31f324b571b3ff1c423ff60e
yu <yu@123>
2023-11-11 16:55:52 +0800

    complete calculator test

--- tests/PriceCalculatorTest.cpp
+++ tests/PriceCalculatorTest.cpp
@@ -4,13 +4,42 @@
 
 using namespace PriceCal;
 
-TEST(PriceCalculator, return_100_when_given_100) {
+
+TEST(PriceCalculator, normal_return_100_when_given_100) {
   // given
   PriceCalculator priceCalculator;
-
   // when
   double cash = priceCalculator.acceptCash(DiscountType::CASS_NORMAL, 100.0);
-
   // then
   EXPECT_DOUBLE_EQ(100, cash);
+}
+
+
+TEST(PriceCalculator, discount_return_90_when_given_100) {
+  // given
+  PriceCalculator priceCalculator;
+  // when
+  double cash = priceCalculator.acceptCash(DiscountType::CASS_DISCOUNT, 100.0);
+  // then
+  EXPECT_DOUBLE_EQ(90, cash);
+}
+
+
+TEST(PriceCalculator, diminish_return_80_when_given_100) {
+  // given
+  PriceCalculator priceCalculator;
+  // when
+  double cash = priceCalculator.acceptCash(DiscountType::CASS_DIMINISH, 100.0);
+  // then
+  EXPECT_DOUBLE_EQ(80, cash);
+}
+
+
+TEST(PriceCalculator, diminish_return_210_when_given_250) {
+  // given
+  PriceCalculator priceCalculator;
+  // when
+  double cash = priceCalculator.acceptCash(DiscountType::CASS_DIMINISH, 250.0);
+  // then
+  EXPECT_DOUBLE_EQ(210, cash);
 }
