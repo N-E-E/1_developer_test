b67edc00dca7d5ba54224af27d8ae7417e2d87f5
yu <yu@123>
2023-11-11 17:06:53 +0800

    finish basic test code

--- tests/PriceCalculatorTest.cpp
+++ tests/PriceCalculatorTest.cpp
@@ -8,8 +8,10 @@ using namespace PriceCal;
 TEST(PriceCalculator, normal_return_100_when_given_100) {
   // given
   PriceCalculator priceCalculator;
+
   // when
   double cash = priceCalculator.acceptCash(DiscountType::CASS_NORMAL, 100.0);
+
   // then
   EXPECT_DOUBLE_EQ(100, cash);
 }
@@ -18,8 +20,10 @@ TEST(PriceCalculator, normal_return_100_when_given_100) {
 TEST(PriceCalculator, discount_return_90_when_given_100) {
   // given
   PriceCalculator priceCalculator;
+
   // when
   double cash = priceCalculator.acceptCash(DiscountType::CASS_DISCOUNT, 100.0);
+
   // then
   EXPECT_DOUBLE_EQ(90, cash);
 }
@@ -28,18 +32,34 @@ TEST(PriceCalculator, discount_return_90_when_given_100) {
 TEST(PriceCalculator, diminish_return_80_when_given_100) {
   // given
   PriceCalculator priceCalculator;
+
   // when
   double cash = priceCalculator.acceptCash(DiscountType::CASS_DIMINISH, 100.0);
+
   // then
   EXPECT_DOUBLE_EQ(80, cash);
+}
+
+
+TEST(PriceCalculator, diminish_return_90_when_given_90) {
+  // given
+  PriceCalculator priceCalculator;
+
+  // when
+  double cash = priceCalculator.acceptCash(DiscountType::CASS_DIMINISH, 90.0);
+  
+  // then
+  EXPECT_DOUBLE_EQ(90, cash);
 }
 
 
 TEST(PriceCalculator, diminish_return_210_when_given_250) {
   // given
   PriceCalculator priceCalculator;
+
   // when
   double cash = priceCalculator.acceptCash(DiscountType::CASS_DIMINISH, 250.0);
+
   // then
   EXPECT_DOUBLE_EQ(210, cash);
 }
