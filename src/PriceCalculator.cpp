f244ca3798be52e827a3913dacea9e2cf77716ca
yu <yu@123>
2023-11-15 12:17:40 +0800

    add discout_seven and discount_eight

--- src/PriceCalculator.cpp
+++ src/PriceCalculator.cpp
@@ -13,10 +13,10 @@ double PriceCalculator::acceptCash(const DiscountType type, const double money) 
       ans = money * 0.9;
       break;
     case DiscountType::CASS_DISCOUNT_EIGHT:
-      ans = money * 0.9;
+      ans = money * 0.8;
       break;
     case DiscountType::CASS_DISCOUNT_SEVEN:
-      ans = money * 0.9;
+      ans = money * 0.7;
       break;
     case DiscountType::CASS_DIMINISH:
       ans = money - std::floor(money / 100.0) * 20;
